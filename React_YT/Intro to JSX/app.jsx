1098fd179e7ad5337229b04a61075becccee42d6
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-10-30 21:16:29 +0530

    Some basics of JSX

---
+++ React_YT/Intro to JSX/app.jsx
@@ -0,0 +1,10 @@
+let h1=<h1> Hello </h1>  // html tag araam se likh sakte
+//Yahi agar js mein karna hota toh kaafi lines ka code likhna padta hai 
+// So JSX -> HTML + JS
+
+//Functions is JS
+function Hello() {
+     console.log("Hello");
+}
+// 1. Function must be capitalized in JSX because in react componets are identified with first alphabet capital
+<Hello /> // We can call the function like this too.
