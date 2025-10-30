a1c487cf0dc2e841aa56d332967baf4740ee6940
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-10-30 20:56:20 +0530

    Importing and exporting variables in JavaScript

---
+++ React_YT/Import And Export/app.js
@@ -0,0 +1,4 @@
+var a=20;
+export let arr=[1,2,3];
+
+export default a; // type 1 of exporting
---
+++ React_YT/Import And Export/script.js
@@ -0,0 +1,4 @@
+import a from './app.js'; // kisi bhi naam se kar sakte ho import like -> import b from './app.js'
+import {arr} from './app.js'; // same name se import karna padega as in the parent file
+console.log(a);
+console.log(arr);
