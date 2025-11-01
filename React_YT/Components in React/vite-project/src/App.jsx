1608e33bbca7e8c8b68fcc4cc0a96d9fb9c36852
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-11-01 20:23:58 +0530

    Understood about Components and how to use them.

---
+++ React_YT/Components in React/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
+      
+    </>
+  )
+}
+
+export default App;
