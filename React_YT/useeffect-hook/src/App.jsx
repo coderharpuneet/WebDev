49443ad82744e2cf0e3296d148e467cc7857a8dd
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-11-20 23:45:40 +0530

    UseEffect on two different buttons

--- React_YT/useeffect-hook/src/App.jsx
+++ React_YT/useeffect-hook/src/App.jsx
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(0);
-  const [num2, setNum2] = useState(0);
+  // const [num, setNum] = useState(0);
+  // const [num2, setNum2] = useState(0);
+  // useEffect(()=>{
+  //   console.log("use effect is running...");
+  // },[num])
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+  function aChanging(){
+    console.log("A ki value change hogi")
+
+  }
+  function bChanging(){
+    console.log("B ki value change hogi")
+  }
   useEffect(()=>{
-    console.log("use effect is running...");
-  },[num])
+    aChanging()
+    console.log("a changing")
+  },[a])
+  useEffect(()=>{
+    bChanging()
+    console.log("b changing")
+  },[b])
   return (
     <div>
-      <h1>Num 1 is: {num}</h1>
+      {/* <h1>Num 1 is: {num}</h1>
       <h1>Num 2 is: {num2}</h1>
       <button 
       onDoubleClick={()=>{
@@ -17,7 +34,20 @@ const App = () => {
       onClick={()=>{
         setNum(num+1)
       }}
-      >Click</button>
+      >Click</button> */}
+
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+      onClick={()=>{
+        setA(a+1)
+      }}
+      >Change A</button>
+      <button
+      onClick={()=>{
+        setA(a+1)
+      }}
+      >Change B</button>
     </div>
   )
 }
