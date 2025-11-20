92a1d9cb92e37fd0fde5b8dea6e1f710d9feeef7
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-11-20 18:05:35 +0530

    Introduction to DOM

---
+++ day 42/script.js
@@ -0,0 +1,6 @@
+var h1=document.querySelector('#heading1');
+var btn=document.querySelector('#btn');
+btn.addEventListener('click',()=>{
+     h1.style.color='red';
+     h1.style.fontSize='60px';
+})
