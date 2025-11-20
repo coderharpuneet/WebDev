604e1b6f866e0b81147865de81871dd759e40040
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-11-20 23:33:40 +0530

    UseEffect concept

--- React_YT/useeffect-hook/src/App.jsx
+++ React_YT/useeffect-hook/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [num2, setNum2] = useState(0);
+  useEffect(()=>{
+    console.log("use effect is running...");
+  },[num])
   return (
-    <div>App</div>
+    <div>
+      <h1>Num 1 is: {num}</h1>
+      <h1>Num 2 is: {num2}</h1>
+      <button 
+      onDoubleClick={()=>{
+        setNum2(num2+1)
+      }}
+      onClick={()=>{
+        setNum(num+1)
+      }}
+      >Click</button>
+    </div>
   )
 }
 
