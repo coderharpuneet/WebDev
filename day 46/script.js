c4ee14f1d9355add680eed4d9ef14fca643d1973
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-12-04 22:02:40 +0530

    Instagram Like Feature in JS

---
+++ day 46/script.js
@@ -0,0 +1,17 @@
+var img=document.querySelector('img');
+var love=document.querySelector('#love');
+
+img.addEventListener('dblclick',()=>{
+     love.style.opacity=1;
+     love.style.transform='translate(-50%,-50%) scale(1) rotate(0deg)';
+     setTimeout(()=>{
+          love.style.transform='translate(-50%,-300%) scale(1) rotate(60deg)';
+     },800)
+     setTimeout(()=>{
+          love.style.opacity=0;
+     },1000)
+     setTimeout(()=>{
+          love.style.transform='translate(-50%,-50%) scale(1) rotate(-60deg)';
+     },1200)
+      
+})
