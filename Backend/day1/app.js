bf248298bb9738f938ca8cc883bb3cbc4ab8e01a
coderharpuneet <harpuneetsingh2006@gmail.com>
2026-06-02 00:43:03 +0530

    Create a server with express

--- Backend/day1/app.js
+++ Backend/day1/app.js
@@ -1,4 +1,16 @@
-const catMe=require("cat-me")
-console.log(catMe())
-console.log(catMe())
-console.log(catMe())
+// const catMe=require("cat-me")
+// console.log(catMe())
+// console.log(catMe())
+// console.log(catMe())
+
+
+const express=require("express")
+const app=express() //Server is created
+const port=3000
+
+//Programming the server
+app.get("/",(req,res)=>{
+     res.send("Hello World")
+})
+
+app.listen(port) //Starts the server
