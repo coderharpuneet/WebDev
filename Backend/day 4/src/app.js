7a7dc1a5505d40515adbd7a59d7f9e22783d1e5d
coderharpuneet <harpuneetsingh2006@gmail.com>
2026-07-12 21:57:54 +0530

    Status codes in https

--- Backend/day 4/src/app.js
+++ Backend/day 4/src/app.js
@@ -22,29 +22,42 @@ app.get("/",(req,res)=>{
 app.post("/notes",(req,res)=>{
      console.log(req.body);
      notes.push(req.body);
+     res.status(201).json({
+          message:"Note created successfully"
+     });
      console.log("All notes!!!!!!!!!!");
      console.log(notes);
-     res.send("Note created successfully");
+     // res.send("Note created successfully");
 });
 
 // GET all Notes
 app.get("/notes",(req,res)=>{
-     res.send(notes);
+     res.status(200).json({
+          message:"All notes fetched successfully",
+          notes:notes
+     });
+     // res.send(notes);
 });
 
 // Delete note
 app.delete("/notes/:id",(req,res)=>{ //id is params 
      const {id}=req.params;
      delete notes[id];
-     res.send("Note deleted successfully");
-     res.send("Update note");
-     res.send(notes);
+     res.status(204).json({
+          message:"Note deleted successfully"
+     });
+     // res.send("Note deleted successfully");
+     
 });
 
 app.patch("/notes/:id",(req,res)=>{
      const {id}=req.params;
      notes[id].description=req.body.description;
-     res.send("Note updated successfully");
-     res.send(notes);
+     res.status(200).json({
+          message:"Note updated successfully",
+          notes:notes
+     });
+     // res.send("Note updated successfully");
+     // res.send(notes);
 });
 module.exports = app;
