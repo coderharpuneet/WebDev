badc92448b2fbd9e2270acf5f08c34e37c37a595
coderharpuneet <harpuneetsingh2006@gmail.com>
2026-07-14 23:39:36 +0530

    Storing mongo uri in dotenv

--- Backend/day 6/src/config/database.js
+++ Backend/day 6/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 function connectDB(){
-     mongoose.connect('mongodb+srv://sidak:fLGGaUPNrbh7zfc8@cluster0.v8z9shl.mongodb.net/day6')
+     mongoose.connect(process.env.MONGO_URI)
      .then(()=>{
           console.log('Database connected');
      })
