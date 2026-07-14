aa0a838e97f519a6a579de84c4137035da745e62
coderharpuneet <harpuneetsingh2006@gmail.com>
2026-07-14 23:35:58 +0530

    Creatig models for storing data

--- Backend/day 6/src/app.js
+++ Backend/day 6/src/app.js
@@ -1,5 +1,20 @@
 const express=require('express');
 const app=express();
+const noteModel=require('./models/notes.model');
 
+app.use(express.json());
+/**
+ * - POST /notes
+ * - req.body => {title, description}
+ */
+app.post('/notes', async (req, res) => {
+     // Logic to create a new note
+     const { title, description } = req.body;
+     const note= await noteModel.create({ title, description })
+     res.status(201).json({ 
+          message: 'Note created successfully', 
+          note 
+     });
+});
 
 module.exports=app;
