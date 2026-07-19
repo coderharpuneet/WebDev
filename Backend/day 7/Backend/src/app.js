c4a948b9f6f84a6807c650c4e5b3a909368f578c
coderharpuneet <harpuneetsingh2006@gmail.com>
2026-07-19 19:13:47 +0530

    Fix JSON response formatting and clean up API endpoint structure for notes

--- Backend/day 7/Backend/src/app.js
+++ Backend/day 7/Backend/src/app.js
@@ -14,9 +14,44 @@ app.use(express.json());
 app.post('/api/notes', async (req, res) => {
     const { title, description } = req.body;
     const newNote = await noteModel.create({ title, description });
-    res.status(201).json(()=>{
-     message: 'Note created successfully',
-     newNote
+    res.status(201).json({
+        message: 'Note created successfully',
+        newNote
+    });
+});
+
+
+/**
+ * - GET /api/notes
+ * - Get all notes from mongodb
+ */
+app.get('/api/notes', async (req, res) => {
+    const notes = await noteModel.find();
+    res.status(200).json(notes);
+})
+
+/**
+ * - DELETE /api/notes/:id
+ * - Delete note by id from mongodb
+ */
+app.delete('/api/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    await noteModel.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Note deleted successfully' });
+}); 
+
+/**
+ * - PATCH /api/notes/:id
+ * - Update note by id from mongodb
+ */
+app.patch('/api/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    const { title, description } = req.body;
+    const updatedNote = await noteModel.findByIdAndUpdate(id, { title, description }, { new: true });
+    res.status(200).json({
+        message: 'Note updated successfully',
+        updatedNote
     });
 });
 
